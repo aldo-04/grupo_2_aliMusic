ebc054769c1998034c003ecbbfed334944084d6e
LucasMouhsen <lucasmouhsen@gmail.com>
2021-07-17 19:18:16 -0300

    cambios en el app y se creo la estructura de archivos

--- sprint2/app.js
+++ sprint2/app.js
@@ -1,9 +1,16 @@
-const express = require("express")
+const express = require("express");
+const app = express();
+const port = 3030;
+const path = require('path');
 
-const app = express()
+app.listen(port,() => console.log("El servidor esta corriendo en http://localhost:"+ port));
 
-const port = 3030
+app.get('/',(req, res) => res.sendfile(path.join(__dirname,'views','index.html')))
 
-app.get('/',(req, res) => res.send('Este es el proyecto del grupo 2'))
+app.get('/login',(req, res) => res.sendfile(path.join(__dirname,'views','login.html')))
 
-app.listen(port,() => console.log("El servidor esta corriendo en http://localhost:"+ port))
+app.get('/productcart',(req, res) => res.sendfile(path.join(__dirname,'views','productCart.html')))
+
+app.get('/productdetail',(req, res) => res.sendfile(path.join(__dirname,'views','productDetail.html')))
+
+app.get('/register',(req, res) => res.sendfile(path.join(__dirname,'views','register.html')))
