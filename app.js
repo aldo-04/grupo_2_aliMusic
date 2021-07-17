ef77eea08b9998e07adb21ce354a7a56c22f1ee4
LucasMouhsen <lucasmouhsen@gmail.com>
2021-07-17 17:44:04 -0300

    Comenzando sprint 2

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-
-const app = express()
-
-const port = 3030
-
-app.get('/',(req, res) => res.send('Este es el proyecto del grupo 2'))
-
-app.listen(port,() => console.log("El servidor esta corriendo en http://localhost:"+ port))
