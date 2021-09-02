7160c0cf7a6755ead05913418e79ed4bf2c8bc3e
JoseMarianoPaz <marianopaz1999@gmail.com>
2021-09-02 19:58:15 -0300

    empiexo users data

--- sitio/src/controllers/userController.js
+++ sitio/src/controllers/userController.js
@@ -1,3 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+const users = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'users.json'),'utf-8'));
+
+const saveUser = dato =>fs.writeFileSync(path.join(__dirname, '..', 'data', 'users.json'),JSON.stringify(products,null,2),'utf-8');
+
 module.exports = {
     user: (req, res) => {
         res.render('users/user',{
@@ -14,6 +21,25 @@ module.exports = {
             title: 'register'
         })
     },
+    proccesRegister:(req,res)=>{
+        const{nombre,apellido,email,celular,password,passworConfirm}=req.body
+
+        userRegister={
+            id: users[users.length -1] ? users[users.length -1].id +1 : 1,
+            nombre : nombre.trim(),
+            apellido : apellido.trim(),
+            email: email.trim(),
+            celular,
+            password,
+            passworConfirm,
+            rol: "user"
+        }
+        return res.send(userRegister)
+        users.push(userRegister);
+        saveUser(users);
+        res.redirect("/");
+    },
+
     fav: (req, res) => {
         res.render('users/fav',{
             title: 'fav' 
--- sitio/src/routes/users.js
+++ sitio/src/routes/users.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var {user, login, register, fav} = require('../controllers/userController')
+var {user, login, register,proccesRegister ,fav} = require('../controllers/userController')
 
+const multer = require('multer');
+const path = require('path')
+
+const storage = multer.diskStorage({
+    destination : (req,file,cb) =>{
+        cb(null, "./src/public/images/fotoUser/")
+    },
+     filename : (req,file,cb) => {
+        cb(null, "user-" + Date.now() + path.extname(file.originalname) )
+    }
+})
+
+const upload = multer({ storage })
 /* GET users listing. */
 router.get('/', user);
 router.get('/login', login);
 router.get('/register', register);
+router.post('/register',upload.single('avatar'),proccesRegister)
 router.get('fav', fav)
 
 module.exports = router;
