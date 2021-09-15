2db796815ceea51cb67e025e2090868550ba1cc9
aldo-04 <ordenaldo@gmail.com>
2021-09-15 04:28:17 -0300

    Muestra en vista de errores al registrarse y arreglos minimos

--- sitio/src/validations/registerValidation.js
+++ sitio/src/validations/registerValidation.js
@@ -6,10 +6,10 @@ module.exports=[
     check("numero").isMobilePhone().withMessage("Debes indicar un numero de telefono valido"),
     check("email").notEmpty().withMessage("Debes indicar tu email"),
     check("password").notEmpty().withMessage("Debes indicar tu contraseña").isLength({max:20,min:6}).withMessage("Tu contraseña debe tener minimo 6 caracteres y maximo 20"),
-    check("password2").notEmpty().custom((value,{req})=>{
+    check("password2").notEmpty().withMessage("Debes repetir tu contraseña").custom((value,{req})=>{
         if(value == req.body.password){
             return true
         }
         return false
-    }).withMessage("La contraseña debe coincidir")
+    }).withMessage("Las contraseñas deben coincidir")
 ]
