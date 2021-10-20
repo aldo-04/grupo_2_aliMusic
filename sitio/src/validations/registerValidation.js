7050eb301b1bfad9b7cb2e5d7511249ddc0e14ea
JoseMarianoPaz <marianopaz1999@gmail.com>
2021-10-20 17:02:02 -0300

    validation

--- sitio/src/validations/registerValidation.js
+++ sitio/src/validations/registerValidation.js
@@ -4,7 +4,7 @@ module.exports=[
     check("firstName").notEmpty().withMessage("Debes indicar tu nombre"),
     check("lastName").notEmpty().withMessage("Debes indicar tu apellido"),
     check("number").isMobilePhone().withMessage("Debes indicar un numero de telefono valido"),
-    check("email").notEmpty().withMessage("Debes indicar tu email"),
+    check("email").isEmail().withMessage("debes ingresar un email").notEmpty().withMessage("Debes indicar tu email"),
     check("password").notEmpty().withMessage("Debes indicar tu contraseña").isLength({max:20,min:6}).withMessage("Tu contraseña debe tener minimo 6 caracteres y maximo 20"),
     check("password2").notEmpty().withMessage("Debes repetir tu contraseña").custom((value,{req})=>{
         if(value == req.body.password){
