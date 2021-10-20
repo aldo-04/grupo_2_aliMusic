759d0b5a0187492fc795f1cde2f08f0f1fbbfe10
LucasMouhsen <lucasmouhsen@gmail.com>
2021-10-20 17:52:35 -0300

    Cambios en el controller

--- sitio/src/controllers/indexcontroller.js
+++ sitio/src/controllers/indexcontroller.js
@@ -1,13 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 const products = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'products.json'),'utf-8'));
+const db = require('../database/models')
 
 module.exports = {
     index: (req, res) => {
-        return res.render('index/index', {
+        db.Product.findAll({
+            include : ['images','productStates']
+        })
+        .then(products =>{
+            res.render('index/index',{
+                title: 'Ali Music',
+                products
+            })
+        })
+        .catch(err=>console.log(err))
+        /* return res.render('index/index', {
             title: 'Ali Music',
             products
-        });
+        }); */
     },
     about: (req, res) => {
         return res.render('index/about',{
--- sitio/src/controllers/productsController.js
+++ sitio/src/controllers/productsController.js
@@ -1,15 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-const products = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'products.json'),'utf-8'));
+/* const fs = require('fs'); */
+/* const path = require('path'); */
+/* const products = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'products.json'),'utf-8')); */
 const capitalizarPrimeraLetra = require("../utils/capitalizeOneLetter.js")
-
+const db = require('../database/models')
 
 module.exports = {
     store: (req, res) => {
-        return res.render('products/store',{
-            title: 'Store', /* Aca se ven todos los productos */
-            products: products,
+        db.Product.findAll({
+            include : ['images','productStates']
         })
+        .then(products =>{
+            return res.render('products/store',{
+                title: 'Store',
+                products
+            })
+        })
+        .catch(err=>console.log(err))
     },
     search: (req, res) => {
         return res.render('products/search',{
@@ -18,34 +24,41 @@ module.exports = {
         })
     },
     detail: (req, res) => {
-        /* db.Product.findAll({
-            order : [
-                ['name','ASC']
-            ],
-            include :[
-                'categories','images','productStates'
-            ]
-        })
-            .then(products => ) */
-        let product = products.find(product => product.id === +req.params.id);
- 
-        return res.render('products/detail',{
-            title: capitalizarPrimeraLetra(product.name), /* Aca se ve el detalle de producto */
-            products: products,
-            product
-        })
+            db.Product.findAll({
+                include : ['images','productStates']
+            })
+            .then(products  =>{
+                let product = products.find(product => product.id === +req.params.id)
+                return res.render('products/detail',{
+                    title: capitalizarPrimeraLetra(product.name),
+                    products : products,
+                    product
+                })
+            })
+            .catch(err=>console.log(err))
     },
-    /* Puede que lo movamos*/
     cart: (req, res) => {
-        return res.render('products/cart',{
-            title: 'Cart', /* Vista carrito */
-            products: products,
+        db.Product.findAll({
+            include : ['images','productStates']
         })
+        .then(products  =>{
+            return res.render('products/cart',{
+                title: 'Cart',
+                products : products
+            })
+        })
+        .catch(err=>console.log(err))
     },
     info: (req, res) => {
-        return res.render('products/infoUser',{
-            title: 'informacion de usuario', /* Vista carrito */
-            products: products,
+        db.Product.findAll({
+            include : ['images','productStates']
+        })
+        .then(products  =>{
+            return res.render('products/infoUser',{
+                title: 'informacion de usuario',
+                products : products
             })
+        })
+        .catch(err=>console.log(err))
         },
-        }
+}
