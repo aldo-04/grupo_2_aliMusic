afe6e2d8333c550f24049a9387c97f50aeaf9446
aldo-04 <ordenaldo@gmail.com>
2021-11-06 20:07:32 -0300

    cambios en validaciones front para crud de productos

--- sitio/public/js/addProduct.js
+++ sitio/public/js/addProduct.js
@@ -11,34 +11,99 @@ window.addEventListener("load",()=>{
 
 
     name.addEventListener("input",()=>{
-        name.value.length <= 0 ? span[0].innerHTML = "<i class='fas fa-times-circle'></i> Debes indicar el nombre del producto" : span[0].innerHTML = ""
+        if (name.value.length <= 0) {
+            span[0].innerHTML = "Debes indicar el nombre del producto"
+            name.classList.add("is-invalid")
+        }else{
+            span[0].innerHTML = ""
+            name.classList.remove("is-invalid")
+            name.classList.add("is-valid")
+        }
     })
     price.addEventListener("input",()=>{
-        price.value <= 0 ? span[1].innerHTML = "<i class='fas fa-times-circle'></i> Debes indicar un precio mayor a 0" : span[1].innerHTML = ""
+        if (price.value <= 0) {
+            span[1].innerHTML = " Debes indicar un precio mayor a 0"
+            price.classList.add("is-invalid")
+        }else{
+            span[1].innerHTML = ""
+            price.classList.remove("is-invalid")
+            price.classList.add("is-valid")
+        }
     })
     discount.addEventListener("input",()=>{
         if(discount.value > 90){
-            span[2].innerHTML = "<i class='fas fa-times-circle'></i> Solo se permiten números hasta 90"
+            span[2].innerHTML = " Solo se permiten números hasta 90"
+            discount.classList.add("is-invalid")
+        }else{
+            span[2].innerHTML = ""
+            discount.classList.remove("is-invalid")
+            discount.classList.add("is-valid")
         }
-        discount.value == "" ? span[2].innerHTML = "" : null
+        if (discount.value < 0) {
+            span[2].innerHTML = " No se permiten números menores a 0"
+            discount.classList.add("is-invalid")
+        }else{
+            discount.classList.add("is-valid")
+        }
     })
     category.addEventListener("input",()=>{
-        category.value.length <= 0 ? span[3].innerHTML = "<i class='fas fa-times-circle'></i> Debes seleccionar una categoria" : span[3].innerHTML = ""
+        category.value.length <= 0 ? span[3].innerHTML = " Debes seleccionar una categoria" : span[3].innerHTML = ""
     })
     description.addEventListener("input",()=>{
-        description.value.length <= 9 ? span[4].innerHTML = "<i class='fas fa-times-circle'></i> La descripción debe tener minimo 10 caracteres" : span[4].innerHTML = ""
+        if (description.value.length <= 9) {
+            span[4].innerHTML = " La descripción debe tener minimo 10 caracteres"
+            description.classList.add("is-invalid")
+        }else{
+            span[4].innerHTML = ""
+            description.classList.remove("is-invalid")
+            description.classList.add("is-valid")
+        }
     })
 
     form.addEventListener("submit",e=>{
 
-        name.value.length <= 0 ? span[0].innerHTML = "<i class='fas fa-times-circle'></i> Debes indicar el nombre del producto" : span[0].innerHTML = ""
+        if (name.value.length <= 0) {
+            span[0].innerHTML = "Debes indicar el nombre del producto"
+            name.classList.add("is-invalid")
+        }else{
+            span[0].innerHTML = ""
+        }
 
-        price.value <= 0 ? span[1].innerHTML = "<i class='fas fa-times-circle'></i> Debes indicar un precio mayor a 0" : span[1].innerHTML = ""
+        if (price.value <= 0) {
+            span[1].innerHTML = " Debes indicar un precio mayor a 0"
+            price.classList.add("is-invalid")
+        }else{
+            span[1].innerHTML = ""
+            price.classList.remove("is-invalid")
+            price.classList.add("is-valid")
+        }
 
-        category.value.length <= 0 ? span[3].innerHTML = "<i class='fas fa-times-circle'></i> Debes seleccionar una categoria" : span[3].innerHTML = ""
+        if(discount.value > 90){
+            span[2].innerHTML = " Solo se permiten números hasta 90"
+            discount.classList.add("is-invalid")
+        }else{
+            span[2].innerHTML = ""
+            discount.classList.remove("is-invalid")
+            discount.classList.add("is-valid")
+        }
+        if (discount.value < 0) {
+            span[2].innerHTML = " No se permiten números menores a 0"
+            discount.classList.add("is-invalid")
+        }else{
+            discount.classList.add("is-valid")
+        }
 
-        description.value.length <= 9 ? span[4].innerHTML = "<i class='fas fa-times-circle'></i> La descripción debe tener minimo 10 caracteres" : span[4].innerHTML = ""
+        category.value.length <= 0 ? span[3].innerHTML = " Debes seleccionar una categoria" : span[3].innerHTML = ""
 
+        if (description.value.length <= 9) {
+            span[4].innerHTML = " La descripción debe tener minimo 10 caracteres"
+            description.classList.add("is-invalid")
+        }else{
+            span[4].innerHTML = ""
+            description.classList.remove("is-invalid")
+            description.classList.add("is-valid")
+        }
+        
         let erroresActivos = []
         span.forEach(error =>{ 
             if (error.textContent != "") {
